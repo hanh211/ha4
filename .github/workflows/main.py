5f42e36067b0c94b62f17f38db9bb572a543a2ad
hanh <hanh@gmail.com>
2023-08-31 17:42:00 +0700

    a

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -2,7 +2,7 @@ from Detector import *
 import os
 
 def main():
-    videoPath="rtsp://admin:admin1234@192.168.1.15:554/cam/realmonitor?channel=1&subtype=0"
+    videoPath="rtsp://admin:admin1234@ngduchanh.ddns.net:554/cam/realmonitor?channel=1&subtype=0"
     configPath=os.path.join("model_data","ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
     modelPath=os.path.join("model_data","frozen_inference_graph.pb")
     # modelPath=os.path.join("model_data","pspnet50+99.pth")
